911cb8449de8781489ad9e756fc2065641a0c0b0
Juan Arroyo <juanarroyobaquerizo2812@outlook.com>
2026-03-09 00:03:41 -0500

    Actualizando microservicio con recepcion de mensajes

--- api/microservicio_rag.py
+++ api/microservicio_rag.py
@@ -3,14 +3,14 @@ from models_llm.RAG import qa_chain
 
 app = FastAPI()
 
-@app.get('/consultar_whatsapp/{pregunta}')
+@app.get('/consultar_whatsapp/{pregunta:path}')
 async def mensaje_whatsapp(pregunta: str):
-    
+    print('Pregunta recibida:', pregunta)
     try:     
         resultado = qa_chain.invoke({'query': pregunta})
         
-        return {'respuesta:': resultado['result']}
+        return {'respuesta': resultado['result']}
     
     except Exception as e:
-        return {f'respuesta:' : 'Lo siento, hubo un error interno en: {str{e}}'}    
+        return {'respuesta:': f'Lo siento, hubo un error interno en: {str(e)}'}    
 
